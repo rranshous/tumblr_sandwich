24a271fa05619ceab3c47395d190670c194b2db2
Robby Ranshous <rranshous@gmail.com>
2015-03-08 16:51:53 -0400

    simple scheduler, starts a container for each blog url

---
+++ scheduler/scheduler.rb
@@ -0,0 +1,33 @@
+require 'httparty'
+require 'uri'
+
+DATA_DIR=File.absolute_path './data'
+
+def run(cmd, opt=nil)
+  puts "running: #{cmd}"
+  unless system(cmd)
+    raise "Error running: #{cmd}" unless opt==:FAILOK
+  end
+end
+
+def docker_run(name, image, options, command, *args)
+  run "docker stop #{name}", :FAILOK
+  run "docker rm #{name}", :FAILOK
+  run "docker run -d --name \"#{name}\" #{options} #{image} #{command} #{args.join(' ')}"
+end
+
+def start_scraper blog_href
+  name = "scrape-#{URI(blog_href).host}"
+  docker_run(name, 'tumblr_sandwich',
+             "-v #{DATA_DIR}:/data",
+             blog_href, '--use-cache')
+end
+
+url = 'https://gist.githubusercontent.com/rranshous/30056e2109f43e74d79f/raw/'
+blog_hrefs = HTTParty.get(url).parsed_response
+  .split("\n").map(&:chomp)
+  .reject{|i|i.start_with?('#')}
+  .reject{|i|i.chomp==''}
+blog_hrefs.each do |blog_href|
+  start_scraper blog_href
+end
