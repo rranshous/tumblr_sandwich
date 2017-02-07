b8d5e876927fd43401bfa4bf428b085b86e6ec76
Robby Ranshous <rranshous@gmail.com>
2017-02-07 12:38:51 -0500

    More debug output, can limit max parallel scrapers, can make run in serial

--- scheduler/start_scraping.rb
+++ scheduler/start_scraping.rb
@@ -4,9 +4,22 @@ require 'uri'
 
 DATA_DIR=ENV['OUTDIR'] || File.absolute_path('./data')
 HREFS_URL=ENV['HREFS_URL']
+MAX_PARALLEL = (ENV['MAX_PARALLEL'] || 10).to_i
 
 $failok = ARGV.include? '--failok'
 $is_full = ARGV.include? '--full'
+$serial = ARGV.include? '--serial'
+$limit_parallel = ARGV.include? '--limit-parallel'
+
+
+puts "starting"
+puts
+puts "failok: #{$failok}"
+puts "full: #{$is_full}"
+puts "serial: #{$serial}"
+puts "limit-parallel: #{$limit_parallel}"
+puts "MAX_PARALLEL: #{MAX_PARALLEL}" if $limit_parallel
+puts
 
 def run(cmd, opt=nil)
   puts " --> running: #{cmd}"
@@ -15,14 +28,22 @@ def run(cmd, opt=nil)
   end
 end
 
+def count_scrapers
+  result = `docker ps | grep scrape- | wc -l`
+  puts "count result: #{result}"
+  return result.to_i
+end
+
 def docker_run(name, image, options, command, *args)
   puts " -> removing old container"
 #  run "docker stop #{name}", :FAILOK
   run "docker rm #{name}", :FAILOK
-  run "docker run -d " + \
-      "-e http_proxy=#{ENV['http_proxy']} " + \
-      "--name \"#{name}\" " + \
-      "#{options} #{image} #{command} #{args.join(' ')}"
+  cmd = "docker run "
+  cmd += "-d " if !$serial
+  cmd += "-e http_proxy=#{ENV['http_proxy']} " + \
+         "--name \"#{name}\" " + \
+         "#{options} #{image} #{command} #{args.join(' ')}"
+  run cmd
 end
 
 def start_scraper blog_href
@@ -42,6 +63,11 @@ blog_hrefs = HTTParty.get(HREFS_URL).parsed_response
   .split("\n").map(&:chomp)
   .reject{|i|i.start_with?('#')}
   .reject{|i|i.chomp==''}
-blog_hrefs.each do |blog_href|
+blog_hrefs.reverse.each do |blog_href|
+  while $limit_parallel && count_scrapers >= MAX_PARALLEL
+    puts 'too many scrapers, sleeping'
+    sleep 5
+  end
   start_scraper blog_href
+  puts
 end
